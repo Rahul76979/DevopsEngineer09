05c6f8b4c3232e09872a9cbdc8893b1ea1732966
divik09 <diviktwr4@gmail.com>
2020-11-22 13:08:20 +0530

    Made dao and edited Dto class by Divik

--- Devops_Practice/src/main/java/com/devops/DTO/UserDTO.java
+++ Devops_Practice/src/main/java/com/devops/DTO/UserDTO.java
@@ -1,5 +1,49 @@
 package com.devops.DTO;
 
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "Devops_db")
 public class UserDTO {
+	
+	@Id
+	private int id;
+	private String name;
+	private String email;
+	private String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "UserDTO [id=" + id + ", name=" + name + ", email=" + email + ", address=" + address + "]";
+	}
+	
 
 }
