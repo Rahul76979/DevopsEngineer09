a72ef4912824b462d10914eb35138b33b4f6aa08
Rahul <rahulsirwani181989@gmail.com>
2020-11-22 15:27:29 +0530

    Edited UserDTO  and Created UserService by Rahul

--- Devops_Practice/src/main/java/com/devops/DTO/UserDTO.java
+++ Devops_Practice/src/main/java/com/devops/DTO/UserDTO.java
@@ -1,10 +1,58 @@
 package com.devops.DTO;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "User_Table")
 public class UserDTO {
 	
     @Id
 	private int id;
+    
+    @Column(name = "FirstName")
+    private String firstName;
+    
+    @Column(name = "LastName")
+    private String lastName;
+    
+    @Column(name = "Email")
+    private String email;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+    
+    
 	
 }
